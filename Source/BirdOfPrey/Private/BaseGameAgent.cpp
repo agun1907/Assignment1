d84c01b4c2e90ca46db130e7369ecb46076da564
agun1907 <157813669+agun1907@users.noreply.github.com>
2026-05-18 15:03:08 +0300

    7

--- Source/BirdOfPrey/Private/BaseGameAgent.cpp
+++ Source/BirdOfPrey/Private/BaseGameAgent.cpp
@@ -1,34 +1,84 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BaseGameAgent.h"
 
-// Sets default values
 ABaseGameAgent::ABaseGameAgent()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+    PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void ABaseGameAgent::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
-// Called every frame
 void ABaseGameAgent::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
-// Called to bind functionality to input
 void ABaseGameAgent::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+void ABaseGameAgent::Reset()
+{
+    Super::Reset();
+}
+
+void ABaseGameAgent::ChangeWeaponType()
+{
+}
+
+FTransform ABaseGameAgent::GetWeaponSpawnTransform()
+{
+    return FTransform::Identity;
+}
+
+void ABaseGameAgent::SpawnDefaultWeapon()
+{
+}
+
+void ABaseGameAgent::StartFire()
+{
+}
+
+void ABaseGameAgent::StopFire()
+{
+}
+
+void ABaseGameAgent::TakeDamage()
+{
+}
+
+bool ABaseGameAgent::IsAlive()
+{
+    return false;
+}
+
+void ABaseGameAgent::Died()
+{
+}
+
+void ABaseGameAgent::PlayHitEffects()
+{
+}
+
+void ABaseGameAgent::CleanUp()
+{
+}
+
+void ABaseGameAgent::ApplyAgentInfo()
+{
+}
+
+void ABaseGameAgent::AimAt()
+{
+}
+
+void ABaseGameAgent::PlayDeathEffects()
+{
+}
+
+void ABaseGameAgent::CheckForOutOfBounds()
+{
+}
--- Source/BirdOfPrey/Private/BaseGameAgent.h
+++ Source/BirdOfPrey/Private/BaseGameAgent.h
@@ -1,29 +1,101 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "BaseGameInstance.h"
+#include "Engine/EngineTypes.h"
 #include "BaseGameAgent.generated.h"
 
+class ABaseWeapon;
+
 UCLASS()
-class ABaseGameAgent : public APawn
+class BIRDOFPREY_API ABaseGameAgent : public APawn
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ABaseGameAgent();
+    ABaseGameAgent();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    virtual void Reset() override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float MoveSpeed;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    ABaseWeapon* Weapon;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    TSubclassOf<ABaseWeapon> DefaultWeaponType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float Health;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FSAgentInfo AgentInfo;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float Points;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    bool bCheckForOutOfBounds;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float OutOfBoundsCheckTolerance;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FName WeaponSocketName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    EAttachmentRule WeaponAttachRule;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FVector TargetLocation;
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void ChangeWeaponType();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    FTransform GetWeaponSpawnTransform();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void SpawnDefaultWeapon();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void StartFire();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void StopFire();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void TakeDamage();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    bool IsAlive();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void Died();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void PlayHitEffects();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void CleanUp();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void ApplyAgentInfo();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void AimAt();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void PlayDeathEffects();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void CheckForOutOfBounds();
 };
