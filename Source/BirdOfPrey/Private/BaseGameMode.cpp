23bfa3c45c0655dddf27e3f9fa7cf6746395753f
agun1907 <157813669+agun1907@users.noreply.github.com>
2026-05-18 14:44:05 +0300

    2

--- Source/BirdOfPrey/Private/BaseGameMode.cpp
+++ Source/BirdOfPrey/Private/BaseGameMode.cpp
@@ -1,5 +1,53 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BaseGameMode.h"
 
+float ABaseGameMode::GetWorldScrollVelocity()
+{
+    return 0.0f;
+}
+
+AActor* ABaseGameMode::GetWorldCameraActor()
+{
+    return nullptr;
+}
+
+void ABaseGameMode::OnPlayerDied()
+{
+}
+
+void ABaseGameMode::OnEnemyDied()
+{
+}
+
+FVector ABaseGameMode::GetPowerUpSpawnLocation()
+{
+    return FVector::ZeroVector;
+}
+
+void ABaseGameMode::StartGame()
+{
+}
+
+void ABaseGameMode::EndGame()
+{
+}
+
+void ABaseGameMode::SpawnEnemyFrom()
+{
+}
+
+void ABaseGameMode::ResetGame()
+{
+}
+
+void ABaseGameMode::RespawnPlayer()
+{
+}
+
+void ABaseGameMode::TrySpawnPowerUp()
+{
+}
+
+float ABaseGameMode::GetDistanceTravelled()
+{
+    return 0.0f;
+}
--- Source/BirdOfPrey/Private/BaseGameMode.h
+++ Source/BirdOfPrey/Private/BaseGameMode.h
@@ -1,17 +1,84 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Engine/DataTable.h"
 #include "BaseGameMode.generated.h"
 
-/**
- * 
- */
+class ABasePowerup;
+
 UCLASS()
-class ABaseGameMode : public AGameModeBase
+class BIRDOFPREY_API ABaseGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float WorldScrollSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FVector2D MaxRelativePlayer;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    AActor* WorldCameraActor;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    UDataTable* PlayerAgentInfoTable;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float RespawnDelay;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float SpawnOffset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FTimerHandle StationarySpawnTimer;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    FTimerHandle ShipSpawnTimer;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    TArray<TSubclassOf<ABasePowerup>> PowerUpList;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    float PickUpSpawnPercentage;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+    bool IsGameOverScreen;
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    float GetWorldScrollVelocity();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    AActor* GetWorldCameraActor();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void OnPlayerDied();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void OnEnemyDied();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    FVector GetPowerUpSpawnLocation();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void StartGame();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void EndGame();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void SpawnEnemyFrom();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void ResetGame();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void RespawnPlayer();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    void TrySpawnPowerUp();
+
+    UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+    float GetDistanceTravelled();
 };
